57501180064f57faf1fa9b3ae62ef6d8c211253b
Your Name <you@example.com>
2025-11-10 16:46:46 +0200

    3 uzdevums

---
+++ coffee-order.js
@@ -0,0 +1,24 @@
+let cofeeOrder ={
+coffeeSize:"large",
+addSugar: true
+
+}
+
+if(cofeeOrder.coffeeSize=="small" && cofeeOrder.addSugar==true )
+    {
+
+    console.log("izmērs maksā 1.20 EUR")
+
+}
+else if(cofeeOrder.coffeeSize=="meduim" && cofeeOrder.addSugar==true )
+    {
+
+    console.log("izmērs maksā 1.7 EUR")
+
+    }
+else(cofeeOrder.coffeeSize=="large" && cofeeOrder.addSugar==true )
+{
+
+    console.log("izmērs maksā 2.20 EUR")
+
+}
